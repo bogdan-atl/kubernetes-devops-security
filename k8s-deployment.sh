19aa3b4b8d523f4be2e6893adf67f2852523bf37
Zykov <60001195+bogdan-atl@users.noreply.github.com>
2023-06-14 23:19:44 +0300

    Update k8s-deployment.sh

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -2,5 +2,5 @@
 
 #k8s-deployment.sh
 
-sed -i 's#REPLACE_ME#docker-registry:5000/java-app:latest#g' k8s_deployment_service.yaml
+sed -i 's#replace#docker-registry:5000/java-app:latest#g' k8s_deployment_service.yaml
 kubectl -n default apply -f k8s_deployment_service.yaml
