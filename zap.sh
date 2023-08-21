49442446e7923b0b9d1039553e5cdbd16919aac6
Zykov <60001195+bogdan-atl@users.noreply.github.com>
2023-08-21 23:25:13 +0300

    Update zap.sh

--- zap.sh
+++ zap.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 PORT=$(kubectl -n default get svc ${serviceName} -o json | jq .spec.ports[].nodePort)
-applicationURL=$(curl ifconfig.me)
 
 # first run this
 chmod 777 $(pwd)
 echo $(id -u):$(id -g)
-docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t http://$applicationURL/v3/api-docs -f openapi -r zap_report.html
+# docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -r zap_report.html
+
+
+# comment above cmd and uncomment below lines to run with CUSTOM RULES
+docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -c zap_rules -r zap_report.html
 
 exit_code=$?
 
+
 # HTML Report
- mkdir -p owasp-zap-report
- mv zap_report.html owasp-zap-report
+ sudo mkdir -p owasp-zap-report
+ sudo mv zap_report.html owasp-zap-report
 
 
 echo "Exit Code : $exit_code"
